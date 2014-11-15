847cf7d423aabb1b71145eb108bc18786d5673f9
saumyakb <saumyakbhatt@yahoo.com>
2014-11-14 20:27:53 -0500

    removed toast messages about wallpaper and setOriginal class

--- AndroidApp/src/edu/cornell/info6130/betterU/setWallpaper.java
+++ AndroidApp/src/edu/cornell/info6130/betterU/setWallpaper.java
@@ -17,7 +17,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.Toast;
 
 public class setWallpaper extends BroadcastReceiver {
 	
@@ -30,17 +29,10 @@ public class setWallpaper extends BroadcastReceiver {
 		    	Bundle bundle=intent.getExtras();
 		    	String[] foodPlateStrArr=bundle.getStringArray("foodPlateBundle");
 		    	String[] mealTimesStrArr=bundle.getStringArray("mealTimesBundle");
-		    	//Log.d("foodPlateStrArr BR",foodPlateStrArr[0]);
-		    	//Log.d("mealTimesStrArr BR",mealTimesStrArr[0]);
 		    	Set<String> foodPlate = new HashSet<String> (Arrays.asList(foodPlateStrArr));
 		    	Set<String> mealTimes = new HashSet<String> (Arrays.asList(mealTimesStrArr));
-		    	
-		   // 	SharedPreferences appPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
-		  //  	Set<String> foodPlate = appPreferences.getStringSet("pref_food_list_key", null);
-		//		Set<String> mealTimes = appPreferences.getStringSet("pref_meal_list_key", null);
-		//		Set<String> snackTimes = appPreferences.getStringSet("pref_snack_list_key", null);
-		
-				// get next priming photo
+		  
+		    	// get next priming photo
 				PhotoManager iPhoto = new PhotoManager(context.getApplicationContext());
 				Drawable displayImage = iPhoto.getPrimingPhoto(foodPlate, mealTimes);
 				
@@ -51,15 +43,11 @@ public class setWallpaper extends BroadcastReceiver {
 			    	String previouslyEncodedImage = sharedPrefOrgWall.getString("imagePreferance", "");
 			    	byte[] b = Base64.decode(previouslyEncodedImage, Base64.DEFAULT);
 			    	Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
-				    myWallpaperManager.setBitmap(bitmap);	
-				    Toast.makeText(context, "Original Wallpaper set sucessfully" ,Toast.LENGTH_SHORT).show();
-		
+				    myWallpaperManager.setBitmap(bitmap);			
 				}
 				else {
 				    final Bitmap wallpaperBitmap = ((BitmapDrawable)displayImage).getBitmap();
 			   		myWallpaperManager.setBitmap(wallpaperBitmap);
-			   		//setResource(R.drawable.salad);
-			   		Toast.makeText(context, "Wallpaper set sucessfully" ,Toast.LENGTH_SHORT).show();
 				}
 		} catch(Exception e){
 			Log.e("Error:", e.toString(), e);
